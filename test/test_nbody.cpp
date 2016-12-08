707ba911a105ff01d89f8a69e5077ea3404aed26
Bastian Hagedorn <b.hagedorn@wwu.de>
2016-12-08 13:06:43 +0100

    added missing includes

--- test/test_nbody.cpp
+++ test/test_nbody.cpp
@@ -3,85 +3,59 @@
 //
 
 #include <cstdio>
+#include <random>
 #include <string>
 
 #include "gtest/gtest.h"
 
-#include "opencl_utils.h"
-#include "nbodyrun.h"
 #include "nbody_harness.h"
+#include "nbodyrun.h"
+#include "opencl_utils.h"
 
-struct NBodyTest: public ::testing::Test {
+struct NBodyTest : public ::testing::Test {
 
-  std::string timing_filename;
+	std::string timing_filename;
 
-  NBodyTest() {
-    OpenCL::init(0, 0);
+	NBodyTest() {
+		OpenCL::init(0, 0);
 
-    timing_filename = File::get_timing_filename();
-  }
+		timing_filename = File::get_timing_filename();
+	}
 
-  virtual void SetUp() {
-    if (File::is_file_exist(timing_filename))
-      std::remove(timing_filename.c_str());
-  }
+	virtual void SetUp() {
+		if (File::is_file_exist(timing_filename)) std::remove(timing_filename.c_str());
+	}
 
-  virtual void TearDown() {
-    if (File::is_file_exist(timing_filename))
-      std::remove(timing_filename.c_str());
-  }
-
+	virtual void TearDown() {
+		if (File::is_file_exist(timing_filename)) std::remove(timing_filename.c_str());
+	}
 };
 
 TEST_F(NBodyTest, CanCreate) {
 
-  auto settings = std::vector<std::string>({
-      "",
-      "1024",
-      "",
-      "1",
-      "16",
-      "",
-      "1",
-      "bla",
-      "0",
-      "0"
-  });
+	auto settings =
+	    std::vector<std::string>({"", "1024", "", "1", "16", "", "1", "bla", "0", "0"});
 
-  auto nBodyRun = new NBodyRun<float>(settings, 1024,0,0,0);
-  EXPECT_NE(nullptr, nBodyRun);
+	auto nBodyRun = new NBodyRun<float>(settings, 1024, 0, 0, 0);
+	EXPECT_NE(nullptr, nBodyRun);
 }
 TEST_F(NBodyTest, Run) {
 
-  auto base_filename = std::string("../../resources/nbody");
-  auto filename = base_filename + ".cl";
+	auto base_filename = std::string("../../resources/nbody");
+	auto filename = base_filename + ".cl";
 
-  ASSERT_TRUE(File::is_file_exist(filename));
+	ASSERT_TRUE(File::is_file_exist(filename));
 
-  size_t size = 256;
-  auto size_string = std::to_string(size);
+	size_t size = 256;
+	auto size_string = std::to_string(size);
 
-  auto settings = std::vector<std::string>({
-      "",
-      size_string,
-      "1",
-      "1",
-      "128",
-      "1",
-      "1",
-      base_filename,
-      "0",
-      "0"
-  });
+	auto settings = std::vector<std::string>(
+	    {"", size_string, "1", "1", "128", "1", "1", base_filename, "0", "0"});
 
-  auto nBodyRun = new NBodyRun<float>(settings, size, 0, 0, 0);
-  auto run = std::vector<std::shared_ptr<::Run>>({std::shared_ptr<::Run>(nBodyRun)});
-  run_harness<float>(
-      run,
-      size, 500.0, 0.005,
-      "testFile1", "testFile2", "testFile3",
-      true, false, false
-  );
+	auto nBodyRun = new NBodyRun<float>(settings, size, 0, 0, 0);
+	auto run = std::vector<std::shared_ptr<::Run>>({std::shared_ptr<::Run>(nBodyRun)});
+	run_harness<float>(run, size, 500.0, 0.005, "testFile1", "testFile2", "testFile3", true,
+			   false, false);
 
-  EXPECT_TRUE(File::is_file_exist(timing_filename));
+	EXPECT_TRUE(File::is_file_exist(timing_filename));
 }
